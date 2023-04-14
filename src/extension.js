c2b6c25e19452703f9ef7c269b5bc3e7a5ffcc86
cvlvxi <cvlvxi@outlook.com>
2023-04-14 23:23:28 +1000

    meh

--- src/extension.js
+++ src/extension.js
@@ -146,7 +146,7 @@ function bootstrap(context) {
 	state.types = new Map([
 		["heading", ["heading", (() => {
 			const getEnlargeDecoration = memoize((size) => vscode.window.createTextEditorDecorationType({
-				textDecoration: `; font-size: ${size/2}px; position: relative; top: 0.1em;`,
+				textDecoration: `; font-size: ${size}px; position: relative; top: 0.1em;`,
 				// textDecoration: `; position: relative; top: 0.1em;`,
 			}));
 			const getlistRainbowDecoration = (() => {
